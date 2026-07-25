8176c295d151ffe250df4756104cba08303c298e
Himanshu Singour <94137280+HimanshuSingour@users.noreply.github.com>
2026-07-26 04:10:18 +0530

    Solve Count Primes

---
+++ AlgorithmXlr8-P004-CountPrimes/Solution.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n <= 2) {
+        cout << 0;
+        return 0;
+    }
+
+    vector<bool> isPrime(n, true);
+
+    isPrime[0] = false;
+    isPrime[1] = false;
+
+    for (int i = 2; i * i < n; i++) {
+
+        if (isPrime[i]) {
+
+            for (int j = i * i; j < n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    int count = 0;
+
+    for (int i = 2; i < n; i++) {
+        if (isPrime[i]) {
+            count++;
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
